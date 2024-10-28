4cb48ffc059af9d974f754a5ff347a8a84a9df31
MickePros <Mikael_Pros@hotmail.com>
2024-10-28 06:27:01 +0100

    Add project files.

---
+++ Storage/Models/ProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Storage.Models
+{
+    public class ProductViewModel
+    {
+        [StringLength(20)]
+        public string Name { get; set; }
+        [Range(1, 50000)]
+        public int Price { get; set; }
+        public int Count { get; set; }
+        public int InventoryValue { get; set; }
+    }
+}
